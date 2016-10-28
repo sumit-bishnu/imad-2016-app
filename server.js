5c43904b37f223bda092ee20ee6146471db887ae
sumit-bishnu <sumitiitmaero@gmail.com>
2016-10-29 01:52:16 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 var articles={
-    'article-ne':{
+    'article-one':{
         title:'Article one by Sumit Kr Bishnu',
         heading:'Article One',
         date:'October 29,2016',
